4e11594b2789fee83146209ad125e4602a9c8101
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2019-09-03 11:37:32 -0300

    tentando salvar arquivo

--- ChatSeguranca/src/main/java/br/edu/unoesc/views/Autenticacao.java
+++ ChatSeguranca/src/main/java/br/edu/unoesc/views/Autenticacao.java
@@ -1,5 +1,17 @@
 package br.edu.unoesc.views;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+import org.apache.commons.io.FileUtils;
+
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.upload.Upload;
@@ -8,25 +20,39 @@ import com.vaadin.flow.router.Route;
 
 @StyleSheet("frontend://styles/styles.css")
 @Route("autenticacao")
-public class Autenticacao extends VerticalLayout{
+public class Autenticacao extends VerticalLayout {
 
 	private static final long serialVersionUID = -8274922127183851458L;
-	
-	public Autenticacao() {
-	    MemoryBuffer buffer = new MemoryBuffer();
-	    Upload upload = new Upload(buffer);
+	private int[] vets;
 
-	    upload.addSucceededListener(event -> {
-	        event.getMIMEType().getBytes();
-	    });
-	    
-	    add(upload);
-	    getStyle().set("align", "center");
-	    getStyle().set("margin-top", "30px");
+	public Autenticacao() {
+		MemoryBuffer buffer = new MemoryBuffer();
+		Upload upload = new Upload(buffer);
+
+		Button b = new Button("enviar");
+
+		add(upload, b);
+
+		b.addClickListener(e -> {
+			buffer.getFileData();
+			String path = System.getProperty("user.home") + File.separator + "Documents" + File.separator
+					+ "certificado";
+			File novoArquivo = new File(path + File.separator + buffer.getFileName());
+			boolean criado;
+
+			try {
+				criado = novoArquivo.createNewFile();
+				FileUtils.writeByteArrayToFile(novoArquivo, buffer.getFileData().getMimeType().getBytes());
+				System.out.println("arquivo criado =========" + criado);
+			} catch (IOException erro) {
+				System.out.println("erro ========" + erro);
+			}
+
+		});
+
+		getStyle().set("align", "center");
+		getStyle().set("margin-top", "30px");
 
 	}
-	
-	
-	
 
 }
--- java-websocket/src/main/java/com/baeldung/websocket/ArquivoController.java
+++ java-websocket/src/main/java/com/baeldung/websocket/ArquivoController.java
@@ -3,31 +3,36 @@ package com.baeldung.websocket;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+
 @Controller
 public class ArquivoController {
 
 	@RequestMapping(value = "/conectar", method = RequestMethod.POST)
-	public void salvar(@RequestParam("file") MultipartFile file){
+	public void salvar(@RequestParam("file") MultipartFile file) throws IOException{
 		
 		/**
-		*	NAO FUNCIONA (REVER)
+		*	ARQUIVO NÃO ESTÁ CHEGANDO (REVER)
 		*/
 		
-		String path = System.getProperty("user.home")+File.separator+"Documents"+File.separator+"certificado";
+		String path = System.getProperty("user.home") + File.separator + "Documents" + File.separator
+				+ "certificado";
 		File novoArquivo = new File(path + File.separator + file.getName());
-		
-        boolean criado;
+		boolean criado;
+
+		byte[] bs = file.getBytes();
 		try {
 			criado = novoArquivo.createNewFile();
+			FileUtils.writeByteArrayToFile(novoArquivo, bs);
 			System.out.println("arquivo criado =========" + criado);
-		} catch (IOException e) {
-			System.out.println("erro ========"+ e);
+		} catch (IOException erro) {
+			System.out.println("erro ========" + erro);
 		}
 	    
 	}
